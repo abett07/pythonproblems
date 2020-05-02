b5580a8de2e031f559d5c773db6bc4d75ea0c046
abett07 <52714630+abett07@users.noreply.github.com>
2020-05-03 00:08:16 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,10 @@
+"""
+18881A0507
+program to print a set containing all colors from color1 which 
+are not present color2
+"""
+color1 = {"yello","blue","green","white","black"}
+color2 = {"blue","green","brown"}
+
+print(color1-(color2))
+ 
---
+++ problem2.py
@@ -0,0 +1,4 @@
+import numpy as np
+#using no.full
+x = np.full((5, 5), 6, dtype=np.uint)
+print(x)
